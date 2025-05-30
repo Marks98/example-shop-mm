202e0d34fedf21e235d4dc1de1e69cbea99bbdda
Markston <mrocklon420@gmail.com>
2025-05-30 22:30:02 +0200

    chore: mobilní zobrazení

--- components/carousel.js
+++ components/carousel.js
@@ -40,14 +40,16 @@ const Carousel = Vue.defineComponent = ({
         }
     },
     template: `
-        <div class="container mx-auto w-full px-[60px] flex items-center justify-end flex-col gap-10 pt-16 pb-8" :class="this.imageUrl">
-            <h1 class="text-white text-[50px] lg:text-[64px] montserrat-800 text-center w-full flex justify-center" v-html="values[index].text"></h1>
-            <div class="hidden sm:flex items-center gap-4">
-                <i class="fas fa-circle cursor-pointer"
-                v-for="value,id in values"
-                :class="{'text-black': id == this.index, 'text-gray-600': id != this.index}"
-                @click="changeSlide(id)"
-                ></i>
+        <div class="w-full px-[60px] h-[400px]" :class="this.imageUrl">
+            <div class="container mx-auto flex items-center justify-center lg:justify-end flex-col gap-10 pt-16 pb-8 h-full">
+                <h1 class="text-white text-[40px] lg:text-[64px] montserrat-800 text-center w-full flex justify-center" v-html="values[index].text"></h1>
+                <div class="hidden sm:flex items-center gap-4">
+                    <i class="fas fa-circle cursor-pointer"
+                    v-for="value,id in values"
+                    :class="{'text-black': id == this.index, 'text-gray-600': id != this.index}"
+                    @click="changeSlide(id)"
+                    ></i>
+                </div>
             </div>
         </div>
     `
